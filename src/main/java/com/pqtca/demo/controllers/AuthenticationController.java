a26ffe987316d555ed1133b66a878f967a1f6c32
Chris Kelley <kelleyc718@gmail.com>
2018-04-27 17:36:53 -0500

    added error pages/bare bones index

--- src/main/java/com/pqtca/demo/controllers/AuthenticationController.java
+++ src/main/java/com/pqtca/demo/controllers/AuthenticationController.java
@@ -1,7 +1,14 @@
 package com.pqtca.demo.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
+    @GetMapping("/hey")
+    public String home() {
+        return "index";
+    }
+
 }
