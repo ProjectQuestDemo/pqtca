e167abe3005187886d46da279ce65a8317ff9d21
Chris Kelley <kelleyc718@gmail.com>
2018-04-27 23:28:38 -0500

    commented code used in css

--- src/main/java/com/pqtca/demo/controllers/AuthenticationController.java
+++ src/main/java/com/pqtca/demo/controllers/AuthenticationController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
-    @GetMapping("/hey")
+    @GetMapping(value = {"/", "/home", "/index", ""})
     public String home() {
-        return "index";
+        return "/index";
     }
 
 }
