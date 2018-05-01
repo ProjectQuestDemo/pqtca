6242298d33856a9cd8351d1af90c59253c3c4d73
alyxandriascott <alyxan.hope26@gmail.com>
2018-05-01 12:08:20 -0500

    updated admin dash, prepped pie chart for implementation

--- src/main/java/com/pqtca/demo/controllers/UserController.java
+++ src/main/java/com/pqtca/demo/controllers/UserController.java
@@ -1,7 +1,30 @@
 package com.pqtca.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping(value = {"/admin"})
+    public String adminDash() {
+        return "admin/admin-dashboard";
+    }
+
+    @GetMapping(value = {"/pending"})
+    public String adminPendingApps() {
+        return "admin/admin-pending";
+    }
+
+    @GetMapping(value = {"/complete"})
+    public String adminCompleteApps() {
+        return "admin/admin-complete";
+    }
+
+    @GetMapping(value = {"/devTeam"})
+    public String adminDevTeam() {
+        return "admin/admin-dev";
+    }
+
+
 }
--- src/main/resources/static/js/dataVis.js
+++ src/main/resources/static/js/dataVis.js
@@ -20,7 +20,7 @@
         ]);
 
         // Optional; add a title and set the width and height of the chart
-        var options = {'title': 'My Average Day', 'width': 400, 'height': 300};
+        var options = {'title': 'My Average Day', 'width': 500, 'height': 300};
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
