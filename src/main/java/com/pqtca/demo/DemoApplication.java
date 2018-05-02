fd653f8a0775701017c1a4534e035b8cb0e5dde7
Chris Kelley <kelleyc718@gmail.com>
2018-05-02 11:18:41 -0500

    fixed main class for prod

--- src/main/java/com/pqtca/demo/DemoApplication.java
+++ src/main/java/com/pqtca/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.pqtca.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,5 +10,9 @@ public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
     }
 }
