93420ed71299cf40b663e9ad9b60f123d6f22b8e
alyxandriascott <alyxan.hope26@gmail.com>
2018-05-03 19:57:27 -0500

    finished portions of pie chart, waiting for db to be updated to complete rest

--- src/main/java/com/pqtca/controllers/ApplicationController.java
+++ src/main/java/com/pqtca/controllers/ApplicationController.java
@@ -22,11 +22,16 @@ public class ApplicationController {
     public String getGraphEthnicity() {
         Gson gson = new Gson();
         HashMap<String, Long> map = new HashMap<>();
-        map.put("white", appDao.countByAEthnicity("white"));
-        map.put("black", appDao.countByAEthnicity("black"));
+        map.put("White", appDao.countByAEthnicity("White"));
+        map.put("Black", appDao.countByAEthnicity("Black"));
+        map.put("Hispanic", appDao.countByAEthnicity("Hispanic"));
+        map.put("Asian/pacific islander", appDao.countByAEthnicity("Asian/pacific islander"));
+        map.put("Other", appDao.countByAEthnicity("Other"));
+        map.put("Two or more", appDao.countByAEthnicity("Two or more"));
+
         return gson.toJson(map);
     }
-// used for pie chart, education graphing
+// used for pie chart, city graphing
     @GetMapping("/graphCity")
     @ResponseBody
     public String getGraphCity() {
@@ -34,6 +39,118 @@ public class ApplicationController {
         HashMap<String, Long> map = new HashMap<>();
         map.put("San Antonio", appDao.countByBCity("San Antonio"));
         map.put("Austin", appDao.countByBCity("Austin"));
+        map.put("Houston", appDao.countByBCity("Houston"));
+        map.put("Dallas", appDao.countByBCity("Dallas"));
+        map.put("Boerne", appDao.countByBCity("Boerne"));
+        map.put("Fredericksburg", appDao.countByBCity("Fredericksburg"));
+        map.put("Castroville", appDao.countByBCity("Castroville"));
+        map.put("Corpus Christi", appDao.countByBCity("Corpus Christi"));
+        map.put("New Braunfels", appDao.countByBCity("New Braunfels"));
+        map.put("Poteet", appDao.countByBCity("Poteet"));
+        map.put("South Padre Island", appDao.countByBCity("South Padre Island"));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart, marital graphing
+    @GetMapping("/graphMarital")
+    @ResponseBody
+    public String getGraphMaritalStatus() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Single", appDao.countByDMaritalStatus("Single"));
+        map.put("Married", appDao.countByDMaritalStatus("Married"));
+        map.put("Separated", appDao.countByDMaritalStatus("Separated"));
+        map.put("Divorced", appDao.countByDMaritalStatus("Divorced"));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart, family stat graphing
+    @GetMapping("/graphFam")
+    @ResponseBody
+    public String getGraphFamilyStatus() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Parent in two parent family", appDao.countByDfamilyStatus("Parent in two parent family"));
+        map.put("Single Parent", appDao.countByDfamilyStatus("Single Parent"));
+        map.put("Other family member", appDao.countByDfamilyStatus("Other family member"));
+        map.put("Non-dependent individual", appDao.countByDfamilyStatus("Non-dependent individual"));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart, drug test graphing
+    @GetMapping("/graphDrug")
+    @ResponseBody
+    public String getGraphDrug() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Consented To Drug Test", appDao.countByEDrugTest(true));
+        map.put("Did Not Consent To Drug Test", appDao.countByEDrugTest(false));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart,federal aid graphing
+    @GetMapping("/graphAid")
+    @ResponseBody
+    public String getGraphFedAid() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Receives TANF", appDao.countByETanf(true));
+        map.put("Receives SNAP", appDao.countByESnap(true));
+        map.put("Receives SSDI", appDao.countByESsdi(true));
+        map.put("Receives SSH", appDao.countByESsh(true));
+        map.put("Receives Unemployment", appDao.countByEUnemp(true));
+        map.put("Receives SSI", appDao.countByESsi(true));
+        map.put("Receives WIC", appDao.countByEWic(true));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart,military graphing
+    @GetMapping("/graphMilitaryService")
+    @ResponseBody
+    public String getGraphMilitaryService() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Army", appDao.countByEMilitarySrv("Army"));
+        map.put("Air Force", appDao.countByEMilitarySrv("Air Force"));
+        map.put("Marine Corps", appDao.countByEMilitarySrv("Marine Corps"));
+        map.put("Coast Guard", appDao.countByEMilitarySrv("Coast Guard"));
+        map.put("Navy", appDao.countByEMilitarySrv("Navy"));
+        map.put("National Guard", appDao.countByEMilitarySrv("National Guard"));
+        map.put("Other", appDao.countByEMilitarySrv("Other"));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart,military spouse graphing
+    @GetMapping("/graphMilitarySpouse")
+    @ResponseBody
+    public String getGraphMilitarySpouse() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Is a Military Spouse", appDao.countByEMilSpouse(true));
+        map.put("Is not a Military Spouse", appDao.countByEMilSpouse(false));
+
+        return gson.toJson(map);
+    }
+
+    // used for pie chart, referral graphing
+    @GetMapping("/graphReferral")
+    @ResponseBody
+    public String getGraphReferral() {
+        Gson gson = new Gson();
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("Agency", appDao.countByEReferralSrc("Agency"));
+        map.put("Job Fair", appDao.countByEReferralSrc("Job Fair"));
+        map.put("Newspaper", appDao.countByEReferralSrc("Newspaper"));
+        map.put("Family/Friend", appDao.countByEReferralSrc("Family/Friend"));
+        map.put("Radio", appDao.countByEReferralSrc("Radio"));
+        map.put("Other", appDao.countByEReferralSrc("Other"));
+
         return gson.toJson(map);
     }
 
--- src/main/java/com/pqtca/repos/ApplicationRepo.java
+++ src/main/java/com/pqtca/repos/ApplicationRepo.java
@@ -2,6 +2,7 @@ package com.pqtca.repos;
 
 import com.pqtca.models.Application;
 import com.pqtca.models.User;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -17,5 +18,28 @@ public interface ApplicationRepo extends CrudRepository<Application, Long> {
     Long countByAEthnicity(String aEthnicity);
 
     Long countByBCity(String bCity);
+
+    Long countByDMaritalStatus(String cMarital);
+
+    Long countByDfamilyStatus(String dFamily);
+
+    Long countByEDrugTest(Boolean eDrugTest);
+
+    Long countByESnap(Boolean eSnap);
+    Long countByESsi(Boolean eSsi);
+    Long countByESsh(Boolean eSsh);
+    Long countByESsdi(Boolean eSsdi);
+    Long countByETanf(Boolean eTanf);
+    Long countByEUnemp(Boolean eUnemp);
+    Long countByEWic(Boolean eWic);
+
+    Long countByEMilitarySrv(String eMilitarySrv);
+
+    Long countByEMilSpouse(Boolean eMilSpouse);
+
+    Long countByEReferralSrc(String eReferralSrc);
+
+
+
 //    TODO will work whenever education is added into the application table.
 }
