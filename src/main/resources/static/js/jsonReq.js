f1cdf2f92a0e57c59b877cc2c329292883edfe10
Chris Kelley <kelleyc718@gmail.com>
2018-05-07 12:57:21 -0500

    changed js for dashboard

--- src/main/resources/static/js/jsonReq.js
+++ src/main/resources/static/js/jsonReq.js
@@ -1,29 +1,34 @@
 'use strict';
 function req(url) {
+    let heading = '';
+    if(url === '/complete') {
+        heading = `<h4>Complete Applications</h4>`;
+    } else {
+        heading = `<h4>Pending Applications</h4>`;
+    }
     fetch(url)
         .then((response) => response.json()
             .then((jsonData) => buildHtml(jsonData))
         );
     const buildHtml = data => {
-        let heading = `<h4>Complete Applications</h4>`;
         let newHtml = '';
         data.forEach(el => newHtml +=
             `<a href="/complete#${el.id}">${el.aFirstName} ${el.aLastName}</a><br/>`);
-        return $('#apps').append(heading + newHtml);
+        return $('#apps').html(heading + newHtml);
     };
 }
 
 
-function preq(url) {
-    fetch(url)
-        .then((response) => response.json()
-            .then((jsonData) => buildHtml(jsonData))
-        );
-    const buildHtml = data => {
-        let heading = `<h4>Pending Applications</h4>`;
-        let newHtml = '';
-        data.forEach(el =>
-            newHtml += `<a href="/pending#${el.id}">${el.aFirstName} ${el.aLastName}</a><br/>`);
-        return $('#apps').append(heading + newHtml);
-    };
-}
+// function preq(url) {
+//     fetch(url)
+//         .then((response) => response.json()
+//             .then((jsonData) => buildHtml(jsonData))
+//         );
+//     const buildHtml = data => {
+//         let heading = `<h4>Pending Applications</h4>`;
+//         let newHtml = '';
+//         data.forEach(el =>
+//             newHtml += `<a href="/pending#${el.id}">${el.aFirstName} ${el.aLastName}</a><br/>`);
+//         return $('#apps').html(heading + newHtml);
+//     };
+// }
