0958180225750507959676d73efec51dabeeb70b
alyxandriascott <alyxan.hope26@gmail.com>
2018-05-12 21:33:58 -0500

    validation updated

--- src/main/resources/static/js/formValidate.js
+++ src/main/resources/static/js/formValidate.js
@@ -145,28 +145,4 @@ function validate6() {
 
 }
 
-function validate7() {
-
-    var x = $(".testinput7");
-
-    var validationArray = [];
-
-    x.each(function () {
-        if ($(this).val() === "" || $(this).val() === null) {
-            $(this).css("border-color", "red");
-            $(this).css("background-color", "red");
-            validationArray.push(false);
-        } else {
-            console.log("is valid");
-            $(this).css("border-color", "inherit");
-            validationArray.push(true);
-        }
-    });
-    console.log(validationArray);
-    return !validationArray.includes(false);
-
-}
-
- // TODO fix validation so that it can be applied to every page, without halting the next button
-
 
